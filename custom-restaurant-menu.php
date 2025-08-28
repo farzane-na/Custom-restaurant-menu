18b0ea2b2addcc63c7804fcf6a23883b012f7ea4
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-08-28 18:14:17 +0330

    init

---
+++ custom-restaurant-menu.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Custom Restaurant Menu
+ * Plugin URI:        https://farzanenazmabadi.ir
+ * Description:       A plugin to create a beautiful and professional restaurant menu
+ * Version:           1.0.0
+ * Requires at least: 7.2
+ * Requires PHP:      7.2
+ * Author:            Farzane Nazmabadi
+ * Author URI:        https://farzanenazmabadi.ir
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ * Requires Plugins:  my-plugin, yet-another-plugin
+ */
