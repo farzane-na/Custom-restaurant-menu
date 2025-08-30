aceb865007d4bdfd143cec55cf1b7c66505d0031
farzane nazmabadi <1379farzanenazmabadi@gmail.com>
2025-08-30 11:15:50 +0330

    creat new widget and initial configurations done.

--- custom-restaurant-menu.php
+++ custom-restaurant-menu.php
@@ -4,13 +4,32 @@
  * Plugin URI:        https://farzanenazmabadi.ir
  * Description:       A plugin to create a beautiful and professional restaurant menu
  * Version:           1.0.0
- * Requires at least: 7.2
- * Requires PHP:      7.2
+ * Requires PHP:      7.4
  * Author:            Farzane Nazmabadi
  * Author URI:        https://farzanenazmabadi.ir
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://example.com/my-plugin/
- * Text Domain:       my-basics-plugin
+ * Update URI:        https://github.com/farzane-na/Custom-restaurant-menu
+ * Text Domain:       restaurant-menu
  * Domain Path:       /languages
- * Requires Plugins:  my-plugin, yet-another-plugin
+ * Requires Plugins:  elementor,woocommerce
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+function translate_plugin() {
+    load_plugin_textdomain( 'farzane-widget', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+
+add_action( 'plugins_loaded', 'translate_plugin' );
+
+
+// Register the widget.
+add_action( 'elementor/init', 'init_custom_elementor_widgets' );
+function init_custom_elementor_widgets() {
+    require_once __DIR__ . '/widgets/menu-items.php';
+
+    add_action( 'elementor/widgets/widgets_registered', 'register_custom_widget_elementor' );
+}
+function register_custom_widget_elementor( $widgets_manager ) {
+    $widgets_manager->register( new \Menu_Items_Widget() );
+}
